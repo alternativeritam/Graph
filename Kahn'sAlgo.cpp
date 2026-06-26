0792ffcc7e5d2883d3a614178475f65ef329992b
Ritam Basu <67322468+alternativeritam@users.noreply.github.com>
2026-06-26 19:49:24 +0530

    Create Kahn'sAlgo.cpp

---
+++ Kahn'sAlgo.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// KAHN'S ALGORITHM
+
+
+int main() {
+    
+    int n;cin>>n;
+    // get the adjacent nodes
+    vector<int> adj[n];
+    vector<int> indegree(n,0);
+    for (int i = 0; i < n; ++i)
+    {
+    	for(auto it:adj[i]){
+    		indegree[it]++;
+    	}
+    }
+    
+    queue<int> q;
+
+    for (int i = 0; i < n; ++i)
+    {
+    	if(indegree[i]==0){
+    		q.push(i);
+    	}
+    }
+
+    vector<int> ans;
+
+    while(!q.empty()){
+
+    	int node = q.front();
+    	q.pop();
+    	ans.push_back(node);
+
+    	for(auto it:adj[node]){
+    		indegree[it]--;
+    		if(indegree[it]==0) q.push(it);
+    	}
+    }
+
+    // print ans
+}
